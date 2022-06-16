e56ed802f966f2aec46c908ed677edcd72a45b9c
matvieira28 <matviera.souza18@gmail.com>
2022-06-16 11:06:26 -0300

    testegit

--- src/test/java/Teste.java
+++ src/test/java/Teste.java
@@ -2,8 +2,38 @@
 public class Teste {
 
 	public static void main(String[] args) {
-		System.out.println("Olá");
+		
+		//objetos= variaveis classes = parte do projeto = projeto = conjunto de classes
 
+		boolean a = true;
+		byte b = 127;
+		short c = 32767;
+		char d = 76;
+		int e = 2147483647;
+		long f = 363373763383838L;
+		float g = 9.292992929e+35F;
+		double h = 1.8829782937827728e+30D;
+		
+		
+		System.out.println( a);
+		System.out.println( b);
+		System.out.println( c);
+		System.out.println( d);
+		System.out.println(e);
+		System.out.println( f);
+		System.out.println( g);
+		System.out.println(h);
+		
+		
+		
+		
+		
+		
+		
+	
+		
+		
 	}
+	
 
 }
